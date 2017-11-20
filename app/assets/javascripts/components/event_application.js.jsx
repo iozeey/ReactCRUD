9f49bf19058dd521e067646b2ba722562d5cfe4d
zeeshan-za-ahmad <zeeshan.za.ahmad@gmail.com>
2017-11-20 18:52:34 +0500

    CRUD Complete

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,9 +1,11 @@
 var EventApplication = createReactClass({
-	getInitialState: function(){
-		return {
-			events:[]
-		};
-	},
+	getInitialState: function() {
+	    return{ 
+	    	events: [],
+	        sort: "name",
+	        order: "asc"
+	    };
+ 	},
 	componentDidMount: function(){
 		this.getDataFromApi();
 	},
@@ -27,6 +29,34 @@ var EventApplication = createReactClass({
 	    events.push(event);
 	    this.setState({ events: events });
   	},
+  	handleDeleteRecord: function(event) {
+	    var events = this.state.events.slice();
+	    var index = events.indexOf(event);
+	    events.splice(index, 1);
+	    this.setState({ events: events });
+  	},
+  	handleUpdateRecord: function(old_event, event) {
+	    var events = this.state.events.slice();
+	    var index = events.indexOf(old_event);
+	    events.splice(index, 1, event);
+	    this.setState({ events: events });
+  	},
+  	handleSortColumn: function(name, order) {
+	    if (this.state.sort != name) {
+	      order = 'asc';
+	    }
+	    $.ajax({
+	      url: '/api/events',
+	      data: { sort_by: name, order: order },
+	      method: 'GET',
+	      success: function(data) {
+	        this.setState({ events: data, sort: name, order: order });
+	      }.bind(this),
+	      error: function(xhr, status, error) {
+	        alert('Cannot sort events: ', error);
+	      }
+	    });
+  	},
 	render: function() {
     return(
       <div className="container">
@@ -43,10 +73,15 @@ var EventApplication = createReactClass({
         	</div>
       	</div>
         <div className="row">
-          <div className="col-md-12">
-          	<EventTable events={this.state.events} />
-          </div>
-        </div>
+        	<div className="col-md-12">
+				<EventTable events={this.state.events}
+						    sort={this.state.sort}
+						    order={this.state.order}
+						    handleDeleteRecord={this.handleDeleteRecord}
+						    handleUpdateRecord={this.handleUpdateRecord} 
+						    handleSortColumn={this.handleSortColumn} />
+				</div>
+        	</div>
       </div>
     )
   }
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,8 @@
 module Api
 	class EventsController < ApplicationController
+		before_action :set_event, only: [:update, :destroy]
 		def index
-      		render json: Event.all
+      		render json: Event.order(sort_by + ' ' + order)
     	end
 
     	def create
@@ -12,6 +13,10 @@ module Api
     			render nothing: true, status: bad_request
     		end
     	end
+    	def destroy
+		    @event.destroy
+		    head :no_content
+    	end
 
     	def search
 		  query = params[:query]
@@ -19,10 +24,32 @@ module Api
 		                       "%#{query}%", "%#{query}%", "%#{query}%")
 		  render json: events
 		end
-		
+
+		def update
+			if @event.update(event_params)
+        		render json: @event
+      		else
+        		render nothing: true, status: :unprocessable_entity
+      		end
+		end
+
 		private
 		def event_params
 			params.require(:event).permit(:name, :description, :event_date, :place)
 		end
+		def set_event
+    		@event = Event.find(params[:id])
+    	end
+
+    	def sort_by
+	      %w(name
+	         place
+	         description
+	         event_date).include?(params[:sort_by]) ? params[:sort_by] : 'name'
+    	end
+
+	    def order
+	      %w(asc desc).include?(params[:order]) ? params[:order] : 'asc'
+	    end
 	end
 end
